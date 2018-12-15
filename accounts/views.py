0762fb7e68c85124a68249bddea6719d77c84658
pratiksinghchauhan <pratiksingh773@gmail.com>
2018-12-15 23:18:41 +0530

    completing transactions

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from models import balance
+from django.contrib.auth.models import User
 
 def signup(request):
     if request.method == 'POST':
@@ -21,5 +22,22 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 def profile(request):
+    msg=""
+    if request.method == "POST":
+        try:
+            username = request.POST["username"]
+            amount = request.POST["amount"]
+            senderUser = User.objects.get(username=request.user.username)
+            receiverrUser =  User.objects.get(username=username)
+            sender = balance.objects.get(user = senderUser)
+            receiverr = balance.objects.get(user = receiverrUser)
+            sender.balance = sender.balance - int(amount)
+            receiverr.balance = receiverr.balance + int(amount)
+            sender.save()
+            receiverr.save()
+            msg = "Transaction Success"
+        except Exception as e:
+            print(e)
+            msg = "Transaction Failure, Please check and try again"
     user = balance.objects.get(user=request.user)
-    return render(request,'profile.html',{"balance":user.balance})
+    return render(request,'profile.html',{"balance":user.balance,"msg":msg})
