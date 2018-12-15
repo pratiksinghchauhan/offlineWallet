47ae54850472eb1baabda0eaa0b726a292f7088b
pratiksinghchauhan <pratiksingh773@gmail.com>
2018-12-15 16:14:08 +0530

    added model for storing balance

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class balance(models.Model):
+    username = models.ForeignKey(User)
+    balance = models.IntegerField(default = 0)
