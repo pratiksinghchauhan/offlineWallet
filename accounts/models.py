70f44df52e85f6b093d89436aa11d47e03ea41d0
pratiksinghchauhan <pratiksingh773@gmail.com>
2018-12-15 16:50:21 +0530

    updating templates

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class balance(models.Model):
     username = models.ForeignKey(User)
