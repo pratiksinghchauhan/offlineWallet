52ab6f506ae72f1790a3cb6ebf1b83e49c2310bd
pratiksinghchauhan <pratiksingh773@gmail.com>
2018-12-15 18:17:38 +0530

    changed templating

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,5 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class balance(models.Model):
-    username = models.ForeignKey(User)
+    user = models.ForeignKey(User)
     balance = models.IntegerField(default = 0)
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from models import balance
 
 def signup(request):
     if request.method == 'POST':
@@ -12,10 +13,13 @@ def signup(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
+            instance = balance(user = request.user, balance = 0)
+            instance.save()
             return redirect('profile')
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
 
 def profile(request):
-    return render(request,'profile.html')
+    user = balance.objects.get(user=request.user)
+    return render(request,'profile.html',{"balance":user.balance})
